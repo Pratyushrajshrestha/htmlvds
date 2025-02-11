22ee6bf46b43218dfa93877a6a75dd5924a9ac39
Pratyush <Pratyush.8719@gmail.com>
2025-02-11 08:00:31 +0545

    assignments

---
+++ assignment/script.js
@@ -0,0 +1,82 @@
+/*let name="Pratyush";
+const age=18;
+var place="Lalitpur";
+
+//console.log("Hello my name is " + name);
+//console.log("My age is " +age);
+
+console.log(name);
+console.log(age);
+console.log(place);
+
+
+let num1 = 10;
+let num2 = 20;
+
+console.log(num1 + num2);
+console.log(num1 > num2);
+console.log(num1 < num2 && num1 > 5);
+
+
+
+//Question
+let numbers = [1, 2, 3, 4, 5,];
+
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
+}
+
+let age;
+
+do {
+  age = prompt("Please enter your age: ");
+} while (isNaN(age) || age <= 0);
+console.log("Thank you for entering a valid age: " + age);
+
+for(i=0;i<=100;i++){
+    if(i%2==0){
+        continue;
+    }
+    console.log(i);
+}
+
+
+let n;
+n= prompt("Enter your name");
+if(n=='pratyush'){
+    console.log("YOU ARE THE OWNER OF THIS SITE");
+}
+else{
+    console.log("YOU ARE NOT A VALID USER");
+}
+
+
+let day;
+day = prompt("Enter number (1-7) to find day");
+day= parseInt(day);
+switch(day){
+    case 1:
+        console.log("Sunday");
+        break;
+    case 2:
+        console.log("Monday");
+        break;    
+    case 3:
+        console.log("Tuesday");
+        break;
+    case 4:
+        console.log("Wednesday");
+        break;
+    case 5:
+        console.log("Thursday");
+        break;    
+    case 6:
+        console.log("Friday");
+        break;
+    case 7:
+        console.log("Saturday");
+        break;
+    default:
+        console.log("Invalid option");
+}
+*/
